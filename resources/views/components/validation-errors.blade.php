7e3b09bd8abc2335104fee3acd8f7f02f6499fc1
Probo Krishnacahya <probo.krishnacahya@gmail.com>
2024-04-18 19:21:25 +0700

    fix: validation-errors styling

--- resources/views/components/validation-errors.blade.php
+++ resources/views/components/validation-errors.blade.php
@@ -1,11 +1,13 @@
 @if ($errors->any())
     <div {{ $attributes }}>
-        <div class="text-danger">{{ __('Whoops! Something went wrong.') }}</div>
+        <div class="alert alert-danger pb-0" role="alert">
+            <p>Whoops! Something went wrong.</p>
 
-        <ul class="text-danger mt-3 list-inside list-disc">
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
+            <ul class="list-inside list-disc">
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
     </div>
 @endif
